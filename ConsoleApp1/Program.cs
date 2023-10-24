87281859c65a2e986ee8474d9712d57e8efb35c5
Branislav Andjelic <branislav.andjelic@ftn.ns>
2023-10-24 11:42:36 +0200

    Implement contains 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Pls give number: ");
+
+string s = Console.ReadLine();
+
+int n = Convert.ToInt32(s);
+bool hasThree = false;
+while (n > 0)
+{
+    if (n % 10 == 3)
+    {
+        hasThree = true;
+        break;
+    }
+    n /= 10;
+}
+if (hasThree)
+{
+    Console.WriteLine("Number contains 3");
+} else
+{
+    Console.WriteLine("Number does not contain 3");
+}
